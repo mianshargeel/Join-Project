3991f861504310e0b0b2edba1641d0a8bc40b7ed
Marian Bohndick <marian.bohndick@gmail.com>
2025-05-07 09:09:20 +0200

    imroved add task dialogue

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { webAppConfig } from './firebase.config';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideFirebaseApp(() => initializeApp(webAppConfig)),
+    provideFirestore(() => getFirestore()),
+    provideRouter(routes),
+  ],
+});
